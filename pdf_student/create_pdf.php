e32bea00fef2877fb2d1550053acbf355b72f69f
zabak79 <rpecko@gmail.com>
2017-02-12 23:22:28 +0100

    test

--- pdf_student/create_pdf.php
+++ pdf_student/create_pdf.php
@@ -1,4 +1,26 @@
 <?php
+    include('../config.php');
+    $id_studenta = $_GET['id_studenta'];
+    $login_id = $_GET['login_id'];
+
+    $dotaz = $pdo->prepare("SELECT *
+                                    FROM studenti
+                                    JOIN tridy ON studenti.trida_id = tridy.id_trida
+                                    JOIN firmy ON studenti.firma_id = firmy.id_firma
+                                    WHERE id_student = :id_student");
+    $vysledek = $dotaz->execute(array(
+        ":id_student" => $id_studenta
+    ));
+    $udaje_studenta = $dotaz->fetch(PDO::FETCH_ASSOC);
+
+    $dotaz = $pdo->prepare("SELECT jmeno, prijmeni FROM ucitele WHERE id_ucitel = :id_ucitel");
+    $vysledek = $dotaz->execute(array(
+        ":id_ucitel" => $login_id
+    ));
+    $udaje_ucitele = $dotaz->fetch(PDO::FETCH_ASSOC);
+
+
+
 
 
 
@@ -9,7 +31,7 @@ $html = '
 <h3>Heading 3</h3>
 <h4>Heading 4</h4>
 <h5>Heading 5</h5>
-<p style="font-kerning: on">Nulla felis erat, imperdiet eu, ullamcorper non, nonummy quis, elit. Suspendisse potenti. Ut a eros at ligula vehicula pretium. Maecenas feugiat pede vel risus. Nulla et lectus. Fusce eleifend neque sit amet erat. Integer consectetuer nulla non orci. Morbi feugiat pulvinar dolor. Cras odio. Donec mattis, nisi id euismod auctor, neque metus pellentesque risus, at eleifend lacus sapien et risus. Phasellus metus. Phasellus feugiat, lectus ac aliquam molestie, leo lacus tincidunt turpis, vel aliquam quam odio et sapien. Mauris ante pede, auctor ac, suscipit quis, malesuada sed, nulla. Integer sit amet odio sit amet lectus luctus euismod. Donec et nulla. Sed quis orci. </p>
+<p style="font-kerning: none">Nulla felis erat, imperdiet eu, ullamcorper non, nonummy quis, elit. Suspendisse potenti. Ut a eros at ligula vehicula pretium. Maecenas feugiat pede vel risus. Nulla et lectus. Fusce eleifend neque sit amet erat. Integer consectetuer nulla non orci. Morbi feugiat pulvinar dolor. Cras odio. Donec mattis, nisi id euismod auctor, neque metus pellentesque risus, at eleifend lacus sapien et risus. Phasellus metus. Phasellus feugiat, lectus ac aliquam molestie, leo lacus tincidunt turpis, vel aliquam quam odio et sapien. Mauris ante pede, auctor ac, suscipit quis, malesuada sed, nulla. Integer sit amet odio sit amet lectus luctus euismod. Donec et nulla. Sed quis orci. </p>
 <h4>Heading using Small-Caps</h4>
 <p>Proin aliquet lorem id felis. Curabitur vel libero at mauris nonummy tincidunt. Donec imperdiet. Vestibulum sem sem, lacinia vel, molestie et, laoreet eget, urna. Curabitur viverra faucibus pede. Morbi lobortis. Donec dapibus. Donec tempus. Ut arcu enim, rhoncus ac, venenatis eu, porttitor mollis, dui. Sed vitae risus. In elementum sem placerat dui. Nam tristique eros in nisl. Nulla cursus sapien non quam porta porttitor. Quisque dictum ipsum ornare tortor. Fusce ornare tempus enim. </p>
 ';
@@ -21,8 +43,7 @@ $html = '
 
 include("../mpdf/mpdf.php");
 
-$id_studenta = $_GET['id_studenta'];
-$login_id = $_GET['login_id'];
+
 
 
 
